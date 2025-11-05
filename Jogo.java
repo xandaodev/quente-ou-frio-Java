4116e7a0993d73622fa906d4c8cd7f0ce27ae571
xandao.dev <alexandrevitaldp@gmail.com>
2025-11-05 10:25:44 -0300

    primeira versao teste do codigo

--- Jogo.java
+++ Jogo.java
@@ -13,13 +13,39 @@ public class Jogo{
     
         while(palpite!= aleatorio){
             palpite = leitor.nextInt();
-            if (palpite == aleatorio){
-                System.out.println("voce acertou o numero!");
-            }else{
-                System.out.println("ta errado paizao tenta de novo ai ");
+            int distancia = palpite - aleatorio;
+            if (distancia<0){
+                distancia = distancia *(-1);
+            }
+            if(distancia >400){
+                System.out.println("ta congelando!");
+            }
+            if(distancia >300 && distancia<400){
+                System.out.println("ta muito frio!");
+            }
+            if(distancia >200 && distancia<300){
+                System.out.println("ta frio ainda!");
+            }
+            if(distancia >100 && distancia<200){
+                System.out.println("ta começando a esquentar!");
+            }
+            if(distancia >50 && distancia<100){
+                System.out.println("ta morno!");
+            }
+            if(distancia >25 && distancia<50){
+                System.out.println("ta quente!");
+            }
+            if(distancia >12 && distancia<25){
+                System.out.println("ta muito quente!");
+            }
+            if(distancia >6 && distancia<12){
+                System.out.println("ta pelando!");
             }
 
         }
+        System.out.println("parabens paizao voce certou o numero!");
+
+        leitor.close();
 
 
 
